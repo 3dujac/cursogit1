0b7a71ff91d68f2114c829a5c1d2be5b7b0b2452
3duj.ac <eduardojosealonso@outlook.es>
2025-06-08 20:29:56 +0200

    primera implementacion del payment

--- paymentt.py
+++ paymentt.py
@@ -1,1 +1,5 @@
-print("Eexecuting paymentt.py")
+print("Esto va a ejectuar el paymentt.py")
+def paymentt():
+    print("Esto es paymentt.py")
+    print("Paymentt ejecutado correctamente")
+    return "Paymentt ejecutado correctamente"
